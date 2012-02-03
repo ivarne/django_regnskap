ddbb73da3c6fb08312c2be5028406359e6092725
Ivar Nesje <ivarne@ivarne.com>
2012-02-03 19:02:36 +0100

    kommet litt videre

--- regnskap/urls.py
+++ regnskap/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'regnskap.views.default'),
+    url(r'^registrer$','regnskap.views.registerform'),
 )
--- regnskap/views.py
+++ regnskap/views.py
@@ -1,1 +1,20 @@
 # Create your views here.
+## standard includes
+import os
+## my files import
+from regnskap import models
+## django import
+from django.shortcuts import render_to_response
+
+
+def default(request):
+    bilag_list = models.Bilag.objects.all()
+    return render_to_response('default.html',{'bilag_list': bilag_list})
+
+def registerform(request):
+    konto_plan = models.Konto.objects.all()
+    files = os.listdir(os.path.join('/','var','www','django_regnskap'))
+    return render_to_response('form.html',{'files': files})
+    
+def registerAction(request):
+    pass
