3f25ae96a911dea9969eceaefff906fc9d03ace8
Ivar Nesje <ivarne@gmail.com>
2012-03-08 13:06:01 +0100

    asdf

--- regnskap/urls.py
+++ regnskap/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
+module = "django_regnskap.regnskap.views."
+
 urlpatterns = patterns('',
 #    url(r'^$', 'regnskap.views.default'),
-    url(r'^registrer$','regnskap.views.bilag.registrerBilagForm'),
-    url(r'^registrer/externalActorJSON$', 'regnskap.views.bilag.ajaxExternalActors'),
-    url(r'^export/(\d{4})/$','regnskap.views.excel.export'),
-    url(r'^dropbox/connect$','regnskap.views.dropbox_integrations.connect'),
-    url(r'^drpobox/export/(\d{4})$', 'regnskap.views.dropbox_integrations.saveBackup'),
-    url(r'^rapport/year/(\d{4})$' , 'regnskap.views.rapport.showYear'),
+    url(r'^registrer$', module + 'bilag.registrerBilagForm'),
+    url(r'^registrer/externalActorJSON$', module + 'bilag.ajaxExternalActors'),
+    url(r'^export/(\d{4})/$', module + 'excel.export'),
+    url(r'^dropbox/connect$', module + 'dropbox_integrations.connect'),
+    url(r'^drpobox/export/(\d{4})$', module + 'dropbox_integrations.saveBackup'),
+    url(r'^rapport/year/(\d{4})$' , module + 'rapport.showYear'),
 )
