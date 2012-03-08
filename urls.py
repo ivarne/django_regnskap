c9f68326e2bba241226a64be9aa1b8350b50be79
Ivar Nesje <ivarne@gmail.com>
2012-03-08 13:02:31 +0100

    sdf

--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^regnskap/', include('regnskap.urls')),
+    url(r'^regnskap/', include('django_regnskap.regnskap.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     
 )
