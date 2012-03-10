21dcbcd4d28985830ceace6d2c5a7343b2d00768
Ivar Nesje <ivarne@gmail.com>
2012-03-10 10:52:29 +0100

    Lagt til robots og favicon.ico

--- regnskap/urls.py
+++ regnskap/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
-module = "django_regnskap.regnskap.views."
 
-urlpatterns = patterns('',
+urlpatterns = patterns('django_regnskap.regnskap.views.',
 #    url(r'^$', 'regnskap.views.default'),
-    url(r'^registrer$', module + 'bilag.registrerBilagForm'),
-    url(r'^registrer/externalActorJSON$', module + 'bilag.ajaxExternalActors'),
-    url(r'^dropbox/connect$', module + 'dropbox_integrations.connect'),
-    url(r'^drpobox/export/(\d{4})$', module + 'dropbox_integrations.saveBackup'),
-    url(r'^rapport/year/(\d{4}).html$' , module + 'rapport.showYear'),
-    url(r'^rapport/year/(\d{4}).xls$' , module + 'excel.export'),
-    url(r'^$', module + 'menues.frontpage'),
+    url(r'^registrer$',                     'bilag.registrerBilagForm'),
+    url(r'^registrer/externalActorJSON$',   'bilag.ajaxExternalActors'),
+    url(r'^dropbox/connect$',               'dropbox_integrations.connect'),
+    url(r'^drpobox/export/(\d{4})$',        'dropbox_integrations.saveBackup'),
+    url(r'^rapport/year/(\d{4}).html$',     'rapport.showYear'),
+    url(r'^rapport/year/(\d{4}).xls$',      'excel.export'),
+    url(r'^$',                              'menues.frontpage'),
 )
