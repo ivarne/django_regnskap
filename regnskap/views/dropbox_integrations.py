ae4486f72439d79ad0b9bc0ade96d903ffb19847
Ivar Nesje <ivarne@ivarne.com>
2016-01-28 21:16:34 +0100

    Added method to copy all bilags to dropbox

--- regnskap/views/dropbox_integrations.py
+++ regnskap/views/dropbox_integrations.py
@@ -5,14 +5,36 @@ django view methods to interact with the dropbox api
 from django_regnskap.regnskap.lib.export import ExelYearView
 
 from django.http import HttpResponse
+from django.conf import settings
 
 from django_regnskap.django_dropbox.decorator import dropbox_user_required
 
+import os
 
 @dropbox_user_required
 def saveBackup(request, year, dropbox_client):
     year = int(year)
-    # import localy so that openpyxl is only required if needed.
+    # Store bilag files
+    db_folder = "/regnskap/%d"%year
+    try:
+        files = dropbox_client.metadata(db_folder)['contents']
+    except Exception as e:
+        if e.status == 404:
+            dropbox_client.file_create_folder(db_folder)
+            files = dropbox_client.metadata(db_folder)['contents']
+        else:
+            return HttpResponse(str(e),status=e.status)
+    files = [f["path"].lower() for f in files]
+    server_folder = os.path.join(settings.MEDIA_ROOT,str(year))
+#    ret = [files]
+    for f in os.listdir(server_folder):
+        path = db_folder+"/"+f
+        if path.lower() not in files:
+            with open(os.path.join(server_folder,f)) as handle:
+#                ret.append(path)
+                dropbox_client.put_file(db_folder + "/" + f, handle)
+    
+    # update the full excel export of the accounting year
     e = ExelYearView(year)
     
     filename = "regnskap/%d-regnskap.xlsx"%year
@@ -21,7 +43,15 @@ def saveBackup(request, year, dropbox_client):
     except Exception:
         metadata = None
     ret = dropbox_client.put_file(filename,e.getExcelFileStream(),parent_rev=metadata)
-#    print ret
+    # update database dump
+    filename = "regnskap/database_dump.sql"
+    try:
+        metadata = dropbox_client.metadata(filename)['rev'];
+    except Exception:
+        metadata = None
+    with open(os.path.join(settings.MEDIA_ROOT,"database_dump.sql")) as file:
+        dropbox_client.put_file(filename,file,parent_rev=metadata)
+
     return HttpResponse(str(ret))
 
 @dropbox_user_required
