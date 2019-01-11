1f9a5deb4f8f5d2f579386fdb02cdca3b82e4d97
Jon Tingvold <jon.tingvold@gmail.com>
2019-01-11 15:06:07 +0000

    Add favicon.ico and robots.txt in urls.py

--- urls.py
+++ urls.py
@@ -6,24 +6,22 @@ from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django_regnskap import regnskap
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_regnskap.views.home', name='home'),
-    # url(r'^django_regnskap/', include('django_regnskap.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^regnskap/', include('django_regnskap.regnskap.urls')),
     url(r'^faktura/',  include('django_regnskap.faktura.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^budsjett/', include('django_regnskap.budsjett.urls')),
     url(r'^lonn/', include('django_regnskap.lonn.urls')),
-    url(r'^$', RedirectView.as_view(url= "/regnskap",permanent=True) )
+    url(r'^$', RedirectView.as_view(url= "/regnskap", permanent=True) ),
+
+    url(r'^(robots\.txt)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,}),
+    url(r'^(favicon\.ico)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,})
 )
 if settings.SERVE_MEDIA_IN_PYTHON:
     urlpatterns += patterns('',
