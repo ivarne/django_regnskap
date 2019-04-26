83c1e3a660a06be78862e492fe79a3130a928825
Jon Tingvold <jon.tingvold@gmail.com>
2019-04-26 19:09:43 +0200

    Update admin.py

--- faktura/admin.py
+++ faktura/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django_regnskap.faktura.models import *
 
 class FakturaAdmin(admin.ModelAdmin):
-    list_display = ('number', 'kunde', 'date', 'frist', 'status', 'prosjekt')
+    list_display = ('getNumber', 'kunde', 'date', 'frist', 'status', 'prosjekt')
     list_filter = ('prosjekt', 'status')
 
 admin.site.register(Faktura, FakturaAdmin)
